b0899f9ca3a8da63c00e45712aea750611c4f668
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2016-06-16 09:05:22 -0400

    Add options object with ignore predicate

--- test/index.spec.js
+++ test/index.spec.js
@@ -18,21 +18,21 @@ describe('createFSALinter()', () => {
 
   it('should console.warn on a non-FSA action', () => {
     const action = { type: 'ACTION', value: 9 }
-    createFSALinter(false)()(next)(action)
+    createFSALinter({ strict: false })()(next)(action)
 
     assert(console.warn.calledOnce)
   })
 
   it('should not try to lint non-objects', () => {
     const action = noop
-    createFSALinter(false)()(next)(action)
+    createFSALinter({ strict: false })()(next)(action)
 
     assert(console.warn.notCalled)
   })
 
   it('should not warn on FSA-compliant actions', () => {
     const action = { type: 'ACTION', payload: 9 }
-    createFSALinter(false)()(next)(action)
+    createFSALinter({ strict: false })()(next)(action)
 
     assert(console.warn.notCalled)
   })
@@ -40,7 +40,27 @@ describe('createFSALinter()', () => {
   it('throw an error on lint failure if strict mode is enabled', () => {
     const action = { type: 'ACTION', value: 9 }
     assert.throws(
-      () => createFSALinter(true)()(next)(action)
+      () => createFSALinter({ strict: true })()(next)(action)
     )
+  })
+
+  it('ignores actions based on a predicate', () => {
+    const action = { type: 'ACTION', value: 9 }
+    const ignoredAction = { type: 'NOPE', value: 1 }
+
+    const ignore = ({ type }) => type === 'NOPE'
+
+    const linter = createFSALinter({ ignore })()(next)
+    linter(action)
+    linter(ignoredAction)
+
+    assert(console.warn.calledOnce)
+  })
+
+  it('defaults to strict: false and a noop ignore', () => {
+    const action = { type: 'ACTION', value: 9 }
+    createFSALinter()()(next)(action)
+
+    assert(console.warn.calledOnce)
   })
 })
